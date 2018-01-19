47e761aea4183b88ef9068b86500a0a9376efb0e
Illia Halych <middlezwei@gmail.com>
2018-01-19 17:39:08 +0200

    Exercise 2.2

--- Chapter-2/Exercise-2-2/main.cpp
+++ Chapter-2/Exercise-2-2/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //upper half
+    for (int row = 1; row <= 4; ++row) {
+
+//        for (int space = 1; space <= ; ++space) {
+//            cout << ' ';
+//        }
+
+        for (int hash = 1; hash <= 2 + 2*(row - 1); ++hash) {
+            cout << '#';
+        }
+
+        cout << endl;
+    }
+
+    //lower half
+    for (int row = 1; row <= 4; ++row) {
+
+//        for (int space = 1; space <= 0; ++space) {
+//            cout << ' ';
+//        }
+
+        for (int hash = 1; hash <= 8 - 2*(row - 1); ++hash) {
+            cout << '#';
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
