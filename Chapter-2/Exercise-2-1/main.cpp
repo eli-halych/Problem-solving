1817f4e2986b8b2d18b7c28003dc8904d4f44bc5
Illia Halych <middlezwei@gmail.com>
2018-01-19 15:39:29 +0200

    Exercise 2.1

---
+++ Chapter-2/Exercise-2-1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main() {
+
+    for (int row = 1; row <= 4; ++row) {
+
+        for (int space = 1; space <= (14%row); ++space) {
+            cout << ' ';
+        }
+
+        for (int hashNum = 1; hashNum <= 2*(5 - row); ++hashNum) {
+            cout << '#';
+        }
+
+        cout << endl;
+
+    }
+
+
+    return 0;
+}
