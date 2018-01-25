8f722da734688f96228cd2419ea155d15998c619
Illia Halych <middlezwei@gmail.com>
2018-01-25 21:33:18 +0200

    Test commits

---
+++ Chapter-3/Exercise-3-5/cmake-build-debug/CMakeFiles/Exercise_3_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise_3_5.dir/main.cpp.o"
+  "Exercise_3_5.pdb"
+  "Exercise_3_5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercise_3_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
