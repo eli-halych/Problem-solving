f881945c292514d99fd59b5c835f09d9eea6e367
Illia Halych <middlezwei@gmail.com>
2018-01-25 22:02:47 +0200

    Check if an ID number of arbitrary length is valid

--- Pure-puzzles/Luhn-checksum-validation.cpp
+++ Pure-puzzles/Luhn-checksum-validation.cpp
@@ -1,3 +1,15 @@
+/*
+ *
+ * 	The Luhn formula is a widely used system for validating identification numbers.
+ * Using the original number, double the value of every other digit. Then add the values of the
+ * individual digits together (if a doubled value now has two digits, add the digits * individually).
+ * The identification number is valid if the sum is divisible by 10.
+ * 	Write a program that takes an identification number of arbitrary length and
+ * determines whether the number is valid under the Luhn formula. The program must
+ * process each character before reading the next one.
+ *
+ */
+
 #include <iostream>
 using std::cin;
 using std::cout;
@@ -20,6 +32,7 @@ int main() {
 
     digit = cin.get();
 
+    // The 10 here means the end of the input, it may differ depending on your system
     while(digit != 10){
         if (position % 2 == 0) { oddLengthChecksum += doubleDigitValue(digit - '0'); evenLengthChecksum += digit -'0';}
         else { oddLengthChecksum += digit -'0'; evenLengthChecksum += doubleDigitValue(digit - '0'); }
