5eee079c6a5188c1773f942753fd0ce3bccd82b9
Illia Halych <middlezwei@gmail.com>
2018-02-04 20:52:35 +0200

    Replace all occurrences of a target string(a sequence of chars) with a new string

--- Pointers-and-dynamic-memory/Replace-string.cpp
+++ Pointers-and-dynamic-memory/Replace-string.cpp
@@ -1,7 +1,8 @@
 /*
  *
- * For dynamically allocated strings(type char* as arrayString), create a function replaceString that takes
- * three parameters, each of type arrayString: source, target, and replaceText.
+ * For dynamically allocated strings(type char* as arrayString), create a function 
+ * replaceString that takes three parameters, each of type arrayString: source, target, 
+ * and replaceText.
  * The function replaces every occurrence of target in source with replaceText.
  * For example, if source points to an array containing abcdabee, target points to
  * ab, and replaceText points to xyz, then when the function ends, source should
@@ -18,7 +19,8 @@ using std::endl;
 typedef char * arrayString;
 
 void replaceString(arrayString& source, arrayString target, arrayString replaceText);
-void append(arrayString &s, int length, arrayString newPiece);
+void append(arrayString &s, char c);
+void concatenate(arrayString &s1, arrayString &s2);
 int length(arrayString s);
 
 
@@ -26,22 +28,19 @@ int length(arrayString s);
 
 int main() {
 
-    arrayString string = new char[9];
-    string[0] = 't', string[1] = 'e', string[2] = 's',
-            string[3] = 't', string[4] = 'e', string[5] = 's',
-            string[5] = 't', string[6] = 'e', string[7] = 's',
-            string[8] = 0;
-    replaceString(string, "te", "-");
+    arrayString string = new char[8];
+    string[0] = 's', string[1] = 't', string[2] = 'e',
+            string[3] = 's', string[4] = 't', string[5] = 'e', string[6] = 's', string[7] = 0;
 
+    replaceString(string, "te", "|");
+
+    cout << "New string: ";
     for (int i = 0; i < length(string); ++i) {
         cout << string[i];
     }cout << endl;
 
     delete[] string; // And here I deallocate this particular string because I no longer need it. Now it is dangling.
     string = NULL; // Not dangling anymore
-
-//    delete[] newString;
-//             newString = NULL;
 
     return 0;
 }
@@ -53,58 +52,75 @@ void replaceString(arrayString& source, arrayString target, arrayString replaceT
 
     int sourceLength = length(source);
     int targetLength = length(target);
-    int replaceTextLength = length(replaceText);
+//    int replaceTextLength = length(replaceText);
+
+    arrayString beforeStr = new char[1];
+    beforeStr[0] = 0;
 
     for (int i = 0; i < sourceLength + 1; ++i) {
-        for (int j = 0; j < targetLength; ++j) {
-            if (source[i+j] == target[j]) {
-                if (j == targetLength-1) { //here's the final point if the target has been found
 
-                    //define lengths for the before and after parts of target
-                    int beforeLength = i;
-                    int afterLength = sourceLength - targetLength - beforeLength;
+        if(source[i] == target[0]){
+            for (int j = 0; j < targetLength; ++j) {
+                    if (j == targetLength - 1){
 
+                        // let the magic happen here
+                        concatenate(beforeStr, replaceText);
+                        i += targetLength - 1;
 
-                    // The before and after arrays
-                    arrayString beforeAndReplaceStr = new char[beforeLength + replaceTextLength + afterLength];
-                    arrayString afterStr = new char[afterLength];
-                    //
-                    for (int c = 0; c < beforeLength; ++c) {
-                        beforeAndReplaceStr[c] = source[c];
                     }
-                    for (int c = (beforeLength + targetLength) - 1; c < sourceLength; ++c) {
-                        afterStr[c] = source[c];
-                    }
-
-
-                    // Append all parts
-                    append(beforeAndReplaceStr, beforeLength, replaceText);
-                    append(beforeAndReplaceStr, beforeLength+replaceTextLength, afterStr);
-
-                    // Delete references
-                    delete[] source;
-                    delete[] afterStr;
-                    source = beforeAndReplaceStr;
-
-
-                }
-                continue;
             }
-            break;
         }
+        else append(beforeStr, source[i]);
     }
+
+    delete[] source;
+    source = beforeStr;
 
 }
 
-void append(arrayString &s, int s_length, arrayString newPiece) {
-    int newPieceLength = length(newPiece);
+void append(arrayString &s, char c)
+{
+    int oldLength = length(s);
 
-    for (int i = s_length, j = 0; j < newPieceLength; i++, j++)
+    arrayString newS = new char[oldLength + 2];
+    for (int i = 0; i < oldLength; i++)
     {
-        s[i] = newPiece[j];
+        newS[i] = s[i];
     }
 
+    newS[oldLength] = c;
+    newS[oldLength + 1] = 0;
+    delete[] s;
+    s = newS;
 }
+
+void concatenate(arrayString &s1, arrayString &s2)
+{
+    int s1_OldLength = length(s1);
+    int s2_Length = length(s2);
+    int s1_NewLength = s1_OldLength + s2_Length;
+
+
+    arrayString newS = new char[s1_NewLength + 1];
+
+    // copy s1 into the beginning of newS
+    for (int i = 0; i < s1_OldLength; i++)
+    {
+        newS[i] = s1[i];
+    }
+    // add s2 to the end of newS
+    for (int i = 0; i < s2_Length; i++)
+    {
+        newS[s1_OldLength + i] = s2[i];
+    }
+    // add the null character to end of newS
+    newS[s1_NewLength] = 0;
+
+    // deallocate s1 from the heap
+    delete[] s1;
+    s1 = newS;
+}
+
 
 int length(arrayString s){
     int count = 0;
